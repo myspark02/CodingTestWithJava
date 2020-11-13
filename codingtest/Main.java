673071822edf524dd060ce172bda94b960329597
scpark <scpark@scparkui-iMac.local>
2020-11-13 14:25:03 +0900

    another commit

--- codingtest/Main.java
+++ codingtest/Main.java
@@ -29,7 +29,80 @@ public class Main {
         //pickOutMiddle();
         // filterElementsDividedBySpecifiedNumber();
         // sumBetweenTwoNumbers();
-        sumOfEveryTwoCombination();
+        // sumOfEveryTwoCombination();
+        sortStringBasedOnNthChar();
+    }
+
+    private static void sortStringBasedOnNthChar() {
+        /*
+            문자열 내 마음대로 정렬하기
+            문제 설명
+            문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 
+            예를 들어 strings가 [sun, bed, car]이고 n이 1이면 각 단어의 인덱스 1의 문자 u, e, a로 strings를 정렬합니다.
+
+            제한 조건
+            strings는 길이 1 이상, 50이하인 배열입니다.
+            strings의 원소는 소문자 알파벳으로 이루어져 있습니다.
+            strings의 원소는 길이 1 이상, 100이하인 문자열입니다.
+            모든 strings의 원소의 길이는 n보다 큽니다.
+            인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
+
+            입출력 예
+            strings	n	return
+            [sun, bed, car]	1	[car, bed, sun]
+            [abce, abcd, cdx]	2	[abcd, abce, cdx]
+            
+            입출력 예 설명
+            
+            입출력 예 1
+            sun, bed, car의 1번째 인덱스 값은 각각 u, e, a 입니다. 이를 기준으로 strings를 정렬하면 [car, bed, sun] 입니다.
+
+            입출력 예 2
+            abce와 abcd, cdx의 2번째 인덱스 값은 c, c, x입니다. 따라서 정렬 후에는 cdx가 가장 뒤에 위치합니다. abce와 abcd는 사전순으로 정렬하면 abcd가 우선하므로, 답은 [abcd, abce, cdx] 입니다.
+        */
+
+        // String[] source = {"sun", "bed", "car"};
+        String[] source = {"abce", "abcd", "cdx"};
+        
+        Scanner input = new Scanner(System.in);
+
+        int kthChar = input.nextInt();
+        
+
+        //selection sort
+        int minIdx = 0;
+        for (int i = 0; i < source.length-1; i++) { 
+            minIdx = i;
+            for (int j = i+1; j < source.length; j++ ) {
+                if (source[j].charAt(kthChar-1) < source[minIdx].charAt(kthChar-1)) {
+                    minIdx = j;
+                } else if (source[j].charAt(kthChar-1) == source[minIdx].charAt(kthChar-1)) { // k번째 문자가 동일하면 사전순으로 앞선 문자열을 찾아야 함.
+                    // alphabet 순으로 앞선 것을 찾자..
+                    // 짤은 문자열의 길이를 len에 저장 
+                    int len = source[j].length() < source[minIdx].length()?source[j].length():source[minIdx].length();
+                    int k = 0;
+                    // 두 문자열에서 같은 문자들은 스킵 
+                    while(k < len && source[j].charAt(k) == source[minIdx].charAt(k)) k++;
+
+                    if (k < len) { // 두 문자열에서 같은 문자는 스킵하고 다른 문자를 비교
+                        if (source[j].charAt(k) < source[minIdx].charAt(k)) {
+                            minIdx = j;
+                        }
+                    } else { // 짧은 길의의 문자열이 긴 길이의 문자열의 부분집합인 경우임. 
+                        if (source[j].length() < source[minIdx].length()) {
+                            minIdx = j;
+                        }
+                    }
+                }
+            }
+            // 찾아진 제일 작은 위치의 값과 i 인덱스 값을 교환 
+            String tmp = source[i];
+            source[i] = source[minIdx];
+            source[minIdx] = tmp;
+        }
+
+        System.out.println(Arrays.toString(source));
+
     }
 
     private static void sumOfEveryTwoCombination() {
