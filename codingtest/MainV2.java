d9d6a2e8019946a214bc72042c67a5c7071f4260
scpark <scpark@scparkui-iMac.local>
2020-11-18 10:29:44 +0900

    another commit(kakako internship, pressing keypad)

--- codingtest/MainV2.java
+++ codingtest/MainV2.java
@@ -13,7 +13,245 @@ public class MainV2 {
         // sumEachDigit();
         // makeArrayFromReversedDigitsOfInteger();
         // sortDigitsInDescendingOrder();
-        ifSquareofNthenCalSquareOfNplusOne();
+        //ifSquareofNthenCalSquareOfNplusOne();
+        // removeMinFromArray();
+        // EvenOrOdd();
+
+        pressKeyPad();
+    }
+
+    private static void pressKeyPad() {
+        /*
+            [카카오 인턴] 키패드 누르기
+
+            1   2   3
+            4   5   6
+            7   8   9
+            *   0   # 
+
+             이 전화 키패드에서 왼손과 오른손의 엄지손가락만을 이용해서 숫자만을 입력하려고 합니다.
+            맨 처음 왼손 엄지손가락은 * 키패드에 오른손 엄지손가락은 # 키패드 위치에서 시작하며, 엄지손가락을 사용하는 규칙은 다음과 같습니다.
+
+            엄지손가락은 상하좌우 4가지 방향으로만 이동할 수 있으며 키패드 이동 한 칸은 거리로 1에 해당합니다.
+            왼쪽 열의 3개의 숫자 1, 4, 7을 입력할 때는 왼손 엄지손가락을 사용합니다.
+            오른쪽 열의 3개의 숫자 3, 6, 9를 입력할 때는 오른손 엄지손가락을 사용합니다.
+            가운데 열의 4개의 숫자 2, 5, 8, 0을 입력할 때는 두 엄지손가락의 현재 키패드의 위치에서 더 가까운 엄지손가락을 사용합니다.
+            4-1. 만약 두 엄지손가락의 거리가 같다면, 오른손잡이는 오른손 엄지손가락, 왼손잡이는 왼손 엄지손가락을 사용합니다.
+            순서대로 누를 번호가 담긴 배열 numbers, 왼손잡이인지 오른손잡이인 지를 나타내는 문자열 hand가 매개변수로 주어질 때, 
+            각 번호를 누른 엄지손가락이 왼손인 지 오른손인 지를 나타내는 연속된 문자열 형태로 return 하도록 solution 함수를 완성해주세요.
+
+            [제한사항]
+            numbers 배열의 크기는 1 이상 1,000 이하입니다.
+            numbers 배열 원소의 값은 0 이상 9 이하인 정수입니다.
+            hand는 "left" 또는 "right" 입니다.
+            "left"는 왼손잡이, "right"는 오른손잡이를 의미합니다.
+            왼손 엄지손가락을 사용한 경우는 L, 오른손 엄지손가락을 사용한 경우는 R을 순서대로 이어붙여 문자열 형태로 return 해주세요.
+            입출력 예
+            numbers	hand	result
+            [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]	"right"	"LRLLLRLLRRL"
+            [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]	"left"	"LRLLRRLLLRR"
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]	"right"	"LLRLLRLLRL"
+            입출력 예에 대한 설명
+            입출력 예 #1
+
+            순서대로 눌러야 할 번호가 [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]이고, 오른손잡이입니다.
+
+            왼손 위치	오른손 위치	눌러야 할 숫자	사용한 손	설명
+            *	#	1	L	1은 왼손으로 누릅니다.
+            1	#	3	R	3은 오른손으로 누릅니다.
+            1	3	4	L	4는 왼손으로 누릅니다.
+            4	3	5	L	왼손 거리는 1, 오른손 거리는 2이므로 왼손으로 5를 누릅니다.
+            5	3	8	L	왼손 거리는 1, 오른손 거리는 3이므로 왼손으로 8을 누릅니다.
+            8	3	2	R	왼손 거리는 2, 오른손 거리는 1이므로 오른손으로 2를 누릅니다.
+            8	2	1	L	1은 왼손으로 누릅니다.
+            1	2	4	L	4는 왼손으로 누릅니다.
+            4	2	5	R	왼손 거리와 오른손 거리가 1로 같으므로, 오른손으로 5를 누릅니다.
+            4	5	9	R	9는 오른손으로 누릅니다.
+            4	9	5	L	왼손 거리는 1, 오른손 거리는 2이므로 왼손으로 5를 누릅니다.
+            5	9	-	-	
+            따라서 "LRLLLRLLRRL"를 return 합니다.
+
+            입출력 예 #2
+
+            왼손잡이가 [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]를 순서대로 누르면 사용한 손은 "LRLLRRLLLRR"이 됩니다.
+
+            입출력 예 #3
+
+            오른손잡이가 [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]를 순서대로 누르면 사용한 손은 "LLRLLRLLRL"이 됩니다.  
+        */
+
+        // int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        // String leftOrRightHanded = "right";
+
+        // int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+        // String leftOrRightHanded = "left";
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String leftOrRightHanded = "right";
+
+        String[] result = new String[numbers.length];
+
+        String whichHand = null;  // 어느 손을 사용할지, 일단 왼손으로 초기화. 
+        int left = -1; // 왼손이 별표에 있음을 의미
+        int right = -1; // 오른손이 별표에 있음을 의미
+
+        for (int i = 0; i < numbers.length; i++) {
+            switch(numbers[i]) {
+                // 3, 6, 9를 누를 땐 오른쪽 엄지 사용.
+                case 3:
+                case 6:
+                case 9:
+                    whichHand = "R";
+                    right = numbers[i]; // 오른 손가락의 마지막 위치 기억
+                    break;
+                /* 2, 5, 8, 0을 누를 땐 현재 왼쪽과 오른쪽 손가락으로부터의 거리를 계산해서 최단거리의 손을 사용. 
+                 거리가 같을 땐, 오른손잡이는 오른손, 왼손잡이는 왼손 사용 */
+                case 2:
+                case 5:
+                case 8:
+                case 0:                    
+                    whichHand = calculateDistance(left, right, leftOrRightHanded, numbers[i]);  
+                    if (whichHand.equals("L")) { // 왼 손가락의 마지막 위치 기억
+                        left = numbers[i];
+                    } else { // 오는 손가락의 마지막 위치 이용
+                        right = numbers[i];
+                    }
+                    break;
+                default : // 1, 4, 7, 0은 왼손
+                    whichHand = "L";
+                    left = numbers[i]; // 왼 손가락의 마지막 위치 기억 
+            }
+
+            result[i] = whichHand;
+        }
+
+        System.out.println(Arrays.toString(result));
+
+    }
+
+    private static String calculateDistance(int left, int right, String leftOrRight, int keyPadNum) {
+        String resultHand = null;
+
+        int distanceFromLeft = 1;
+        int distanceFromRight = 1;
+
+        // 눌러야할 키패드가 0, 1, 2, 3, ... 9일 경우, 그 키패드로부터 0, 1, 2, 3, ... 9, *, # 까지의 거리를 저장.
+        // 눌러야 할 키패드가 0이고 현재 손가락의 위치가 5라면, distances[0][5]에 그 거리가 있음. 
+        // 눌러야 할 키패드가 0이고 현재 손가락의 위치가 *이면, distances[0][10]에 그 거리가 있음
+        // 눌러야 할 키패드가 0이고 현재 손가락의 위치가 #이면, distances[0][11]에 그 거리가 있음
+        int[][] distances = {{0, 4, 3, 4, 3, 2, 3, 2, 1, 2, 1, 1}, {4, 0, 1, 2, 1, 2, 3, 2, 3, 4, 3, 5}, 
+                             {3, 1, 0, 1, 2, 1, 2, 3, 2, 3, 4, 4}, {4, 2, 1, 0, 2, 2, 1, 4, 3, 2, 5, 3}, 
+                             {3, 1, 2, 3, 0, 1, 2, 1, 2, 3, 2, 4}, {2, 2, 1, 2, 1, 0, 1, 2, 1, 2, 3, 3}, 
+                             {3, 3, 2, 1, 2, 1, 0, 3, 2, 1, 4, 2}, {2, 2, 3, 4, 1, 2, 3, 0, 1, 2, 1, 3}, 
+                             {1, 3, 2, 3, 2, 1, 2, 1, 0, 1, 2, 2}, {2, 4, 3, 2, 3, 2, 1, 2, 1, 0, 3, 1} , 
+
+                        }; 
+   
+        if (left == -1) { //왼 손이 *에 있다. 
+            left = 10;
+        }             
+        if (right == -1) { //오른 손이 #에 있다. 
+            right = 11;
+        }   
+
+        distanceFromLeft = distances[keyPadNum][left];
+        distanceFromRight = distances[keyPadNum][right];
+        
+        System.out.println("keyPad: " + keyPadNum + ", left:" + left + ", right:" + right);
+        System.out.println("distanceFromLeft: " + distanceFromLeft + ", distanceFromRight:" + distanceFromRight) ;
+        System.out.println("\n");
+
+        if (distanceFromLeft < distanceFromRight) 
+                        resultHand = "L";
+        else if (distanceFromLeft > distanceFromRight)
+                        resultHand = "R";
+        else { // 거리가 같으면 왼손잡이, 오른손잡이에 따라 결정
+            if (leftOrRight.equals("left"))
+                resultHand="L";
+            else 
+                resultHand="R";
+        }
+
+        return resultHand;
+    }
+
+
+    private static void EvenOrOdd() {
+        /*
+            짝수와 홀수
+            문제 설명
+            정수 num이 짝수일 경우 Even을 반환하고 홀수인 경우 Odd를 반환하는 함수, solution을 완성해주세요.
+
+            제한 조건
+            num은 int 범위의 정수입니다.
+            0은 짝수입니다.
+            입출력 예
+            num	return
+            3	Odd
+            4	Even
+
+        */
+        int n = 3;
+        n = 4;
+
+        if (n%2==0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+    private static void removeMinFromArray() {
+        /*
+            제일 작은 수 제거하기
+            문제 설명
+            정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 
+            단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 
+            예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+
+            제한 조건
+            arr은 길이 1 이상인 배열입니다.
+            인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+            입출력 예
+            arr	return
+            [4,3,2,1]	[4,3,2]
+            [10]	[-1]
+
+        */
+
+
+        // int[] arr = {4, 3, 2, 1};
+        // int[] arr = {10};
+        int[] arr = {5, 398, 23, 523, 90, 324, 4};
+        int[] result = null;
+        if (arr.length <= 1) {
+            result = new int[1];
+            result[0] = -1;
+            System.out.println(Arrays.toString(result));
+            return;    // 메소드 종료 
+        } else {  // 결과 배열의 크기는 원래 배열의 크기 - 1 
+            result = new int[arr.length-1];
+        }
+
+        // 배열에서 제일 작은 값의 인덱스 찾기.
+        int minIdx = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[minIdx] > arr[i]) {
+                minIdx = i;
+            }
+        }
+        int idx = 0; // 결과 배열의 인덱스 
+        // 최소값을 제외한 나머지 arr배열의 원소를 result 배엘에 복사 
+        for (int i = 0; i < arr.length; i++) {
+            if (i != minIdx) {
+                result[idx++] = arr[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(result));
+
+
     }
 
     private static void ifSquareofNthenCalSquareOfNplusOne() {
