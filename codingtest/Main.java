bf8efc3e5b6606bfec78542c99aff1b522950a3a
scpark <scpark@scparkui-iMac.local>
2020-11-26 20:39:53 +0900

    Pull up Dolls game

--- codingtest/Main.java
+++ codingtest/Main.java
@@ -38,6 +38,130 @@ public class Main {
         // findPrimeNumberCount();
         // makeSuBakString();
         // str2num();
+        dollPickupGame();
+    }
+
+    private static void dollPickupGame() {
+        /*
+        크레인 인형뽑기 게임
+        문제 설명
+        게임개발자인 죠르디는 크레인 인형뽑기 기계를 모바일 게임으로 만들려고 합니다.
+        죠르디는 게임의 재미를 높이기 위해 화면 구성과 규칙을 다음과 같이 게임 로직에 반영하려고 합니다.
+
+        crane_game_101.png
+
+        게임 화면은 1 x 1 크기의 칸들로 이루어진 N x N 크기의 정사각 격자이며 위쪽에는 크레인이 있고 오른쪽에는 바구니가 있습니다. 
+        (위 그림은 5 x 5 크기의 예시입니다). 각 격자 칸에는 다양한 인형이 들어 있으며 인형이 없는 칸은 빈칸입니다. 
+        모든 인형은 1 x 1 크기의 격자 한 칸을 차지하며 격자의 가장 아래 칸부터 차곡차곡 쌓여 있습니다. 
+        게임 사용자는 크레인을 좌우로 움직여서 멈춘 위치에서 가장 위에 있는 인형을 집어 올릴 수 있습니다. 
+        집어 올린 인형은 바구니에 쌓이게 되는 데, 이때 바구니의 가장 아래 칸부터 인형이 순서대로 쌓이게 됩니다. 
+        다음 그림은 [1번, 5번, 3번] 위치에서 순서대로 인형을 집어 올려 바구니에 담은 모습입니다.
+
+        crane_game_102.png
+
+        만약 같은 모양의 인형 두 개가 바구니에 연속해서 쌓이게 되면 두 인형은 터뜨려지면서 바구니에서 사라지게 됩니다. 
+        위 상태에서 이어서 [5번] 위치에서 인형을 집어 바구니에 쌓으면 같은 모양 인형 두 개가 없어집니다.
+
+        crane_game_103.gif
+
+        크레인 작동 시 인형이 집어지지 않는 경우는 없으나 만약 인형이 없는 곳에서 크레인을 작동시키는 경우에는 아무런 일도 일어나지 않습니다. 
+        또한 바구니는 모든 인형이 들어갈 수 있을 만큼 충분히 크다고 가정합니다. (그림에서는 화면표시 제약으로 5칸만으로 표현하였음)
+
+        게임 화면의 격자의 상태가 담긴 2차원 배열 board와 인형을 집기 위해 크레인을 작동시킨 위치가 담긴 배열 moves가 매개변수로 주어질 때, 
+        크레인을 모두 작동시킨 후 터트려져 사라진 인형의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+        [제한사항]
+        board 배열은 2차원 배열로 크기는 5 x 5 이상 30 x 30 이하입니다.
+        board의 각 칸에는 0 이상 100 이하인 정수가 담겨있습니다.
+        0은 빈 칸을 나타냅니다.
+        1 ~ 100의 각 숫자는 각기 다른 인형의 모양을 의미하며 같은 숫자는 같은 모양의 인형을 나타냅니다.
+        moves 배열의 크기는 1 이상 1,000 이하입니다.
+        moves 배열 각 원소들의 값은 1 이상이며 board 배열의 가로 크기 이하인 자연수입니다.
+        입출력 예
+        board	moves	result
+        [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]	[1,5,3,5,1,2,1,4]	4        
+        */
+
+        int[][] board =  {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4};
+        int answer = 0;
+        /*
+             0 0 0 0 0 
+             0 0   0  
+             0   5 0 
+               2 4 4 2
+               5 1 3 1
+        */ 
+        // 1 5 3 5 1 2 1 4
+        // 4 3 1 1 3 2 4
+        
+        int[] bag = new int[moves.length]; // 바구니에 들어가는 인형의 최대수는 크레인이 움직이는 수니까.
+
+        
+        /*
+            Step 1.
+            인형이 들어있는 칸마다 마지막 위치를 기억.
+
+            Step 2.
+            배열 moves의 각 원소에 대해 
+                그 원소가 가리키는 칸에서 인형을 꺼내 bag에 넣는다. 0이면 넣지 않는다. 
+                그 칸의 마지막 위치 포인터를 1 감소 시킨다.
+            Step 3.
+            bag에 있는 원소 각각에 대해
+                연속된 값이 두 개가 나오면 두 개를 터뜨리고
+                 터진 갯수를 1증가 시키고, 
+                
+        */
+        // Step 1. 
+        int[] lastPositions = new int[board.length];
+        for (int i  = 0; i < lastPositions.length; i++)
+            lastPositions[i] = 0;  // 해당 칸의 마지막 위치를 기억.
+        
+        System.out.println("lastPositions:" + Arrays.toString(lastPositions));
+
+        // Step 2.
+        int bagIdx = 0;
+        for (int i = 0; i < moves.length; i++) {
+            while(lastPositions[moves[i]-1] < board.length && board[lastPositions[moves[i]-1]][moves[i]-1] == 0) 
+                lastPositions[moves[i]-1] += 1;
+            if (lastPositions[moves[i]-1] >= board.length) continue; // 이 칸에서는 더 이상 없다. 
+            int doll = board[lastPositions[moves[i]-1]][moves[i]-1];
+            if (doll != 0) {
+                bag[bagIdx++] = doll;
+            }
+            lastPositions[moves[i]-1] += 1; // 해당 칸의 마지막 위치를 1 감소 시킨다. 
+        }
+
+        System.out.println("bag:" + Arrays.toString(bag));
+        System.out.println("lastPositions:" + Arrays.toString(lastPositions));
+
+        // Step 3.  
+        int previous = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < bag.length; i++) {
+            if (bag[i] == 0) break;
+            list.add(bag[i]);
+        }
+        System.out.println(list);
+        while(previous < list.size() - 1) {
+            if (list.get(previous) == list.get(previous+1)) {
+                answer+=2;
+                list.remove(previous);
+                list.remove(previous);
+                previous -= 1;
+                if (previous < 0) previous = 0;
+            } else {
+                previous = previous + 1;
+            }
+           
+            // System.out.println("\nlist.size():" + list.size());
+            // System.out.println(list);
+            // System.out.println("previous:" + previous);
+        }
+
+      
+        // System.out.println("bag:" + Arrays.toString(bag));
+        System.out.println(answer);
     }
 
     private static void str2num() {
