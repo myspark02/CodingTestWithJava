44db0872d19d90688d417e2096bcfe99245db3d3
myspark02 <myspark02@gmail.com>
2021-03-10 21:04:11 +0900

    Thread basic

---
+++ codingwithscpark/yju/thread/CarGame.java
@@ -0,0 +1,62 @@
+package codingwithscpark.yju.thread;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CarGame extends JFrame{
+    private int rank = 1;
+    public static void  main(String[] args) {
+        new CarGame();
+    }
+    public CarGame() {
+        this.setTitle("Car Race");
+        this.setSize(600, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+
+        (new CarThread("car1.jpeg", 0, 0)).start();
+        (new CarThread("car2.jpeg", 0, 100)).start();
+        (new CarThread("car3.jpeg", 0, 200)).start();
+
+        this.setVisible(true);
+    }
+
+    class CarThread extends Thread {
+        private JLabel label;
+        private int x, y;
+        private String fileName;
+        public CarThread(String fileName, int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.fileName = fileName;
+            label = new JLabel();
+            label.setIcon(new ImageIcon(fileName));
+            label.setBounds(x, y, 100, 100);
+            add(label);
+        }
+        @Override
+        public void run() {
+            System.out.println("Active thread count:" + Thread.activeCount());
+            for (int i = 0; i < 600; i++) {
+                x += 10 * Math.random();
+                // x = x + (10*Math.random());
+                // System.out.println("x:" + x +", y:" + y);
+                label.setBounds(x, y, 100, 100);
+                repaint();
+                if (x >= 500) {
+                    System.out.println(fileName.substring(0, fileName.indexOf("."))+" 도착 " + (rank++) + "등!!!");
+                    break;
+                }
+                try {
+                    Thread.sleep(100);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            
+        }
+}
+
+
+
+}
---
+++ codingwithscpark/yju/thread/ThreadExecutor.java
@@ -0,0 +1,23 @@
+package codingwithscpark.yju.thread;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class ThreadExecutor {
+    public static void main(String[] args) {
+        Runnable r1 = () -> {
+            for (int i = 0; i <= 10; i++) 
+                System.out.println("첫 번째 쓰레드:" + i);
+        };
+
+        Runnable r2 = () -> {
+            for (int i = 10; i >= 0; i--) 
+                System.out.println("두 번째 쓰레드:" + i);
+        };
+
+        Executor executor = Executors.newCachedThreadPool();
+        executor.execute(r1);
+        executor.execute(r2);
+    }
+    
+}
