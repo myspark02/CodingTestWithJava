8be05e651942463232b2c27331e09d298c22b6d3
myspark02 <myspark02@gmail.com>
2021-03-16 00:49:24 +0900

    streams, enum

---
+++ codingwithscpark/enumtest/EnumDemo.java
@@ -0,0 +1,19 @@
+package codingwithscpark.enumtest;
+
+public class EnumDemo {
+
+    public static void main(String[] args) {
+        Mobile m = Mobile.LG;
+        System.out.println(m);
+        System.out.println(m.getVendor());
+        System.out.println(Mobile.APPLE);
+        System.out.println(Mobile.SAMSUNG);
+        System.out.println(Mobile.LG);
+
+        switch (m) {
+        case APPLE:
+        case SAMSUNG:
+        case LG:
+        }
+    }
+}
---
+++ codingwithscpark/enumtest/Mobile.java
@@ -0,0 +1,15 @@
+package codingwithscpark.enumtest;
+
+public enum Mobile {
+    APPLE("apple"), SAMSUNG("samsung"), LG("lg");
+
+    private final String vendor;
+
+    private Mobile(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+}
